0c605f3131ee25a7daedc2c8e56139ecad48597b
justinaus <justinaus2k@gmail.com>
2019-10-30 18:59:39 +0900

    Update google search console meta tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `google-site-verification`,
+          content: `Bn6V-JabYPU85bVACEySDeO6pQoRWVm5PXGPqc4k-Q8`,
+        },
       ].concat(meta)}
     />
   )
